ebc29b7a348bc5eb69585c78fbb434c847bd79a2
jason.m.nance <jason.m.nance@gmail.com>
2019-02-26 22:05:29 -0500

    fix data export, add Jupyter setup

--- export_data.sh
+++ export_data.sh
@@ -6,6 +6,6 @@ docker-compose up -d
 docker-compose exec database bash -c '
                psql \
                -U "$POSTGRES_USER" \
-               -c "\copy (SELECT * FROM analysis_view) TO /data/traffic_stops_nc.csv WITH CSV;"
+               -c "\copy (SELECT * FROM analysis_view) TO /data/traffic_stops_nc.csv WITH CSV HEADER;"
 '
 xz data/traffic_stops_nc.csv --stdout > traffic_stops_nc.csv.xz
